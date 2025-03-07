aaa05e2e2ce34fa6fadaa4e5663694a336bc0a5d
Dhruv Agarwal <dhruv9837758333@gmail.com>
2025-03-07 10:59:16 +0530

    Added the functionality of storing the data in database

--- src/main/java/com/example/AddressBook/controllers/AddressController.java
+++ src/main/java/com/example/AddressBook/controllers/AddressController.java
@@ -6,6 +6,8 @@ import com.example.AddressBook.interfaces.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/addressbook")
 public class AddressController {
@@ -66,6 +68,39 @@ public class AddressController {
     public ResponseDTO delete2(@PathVariable Long id){
         return iEmployeeService.response("API triggered at /res/delete/{id}", "Success");
     }
+
+    //UC3 --> Handling REST API using service layer with storage in database
+
+    @GetMapping("res1/get/{id}")
+    public EmployeeDTO get3(@PathVariable Long id){
+        return iEmployeeService.get(id);
+    }
+
+    @PostMapping("res1/add")
+    public EmployeeDTO create3(@RequestBody EmployeeDTO user){
+        return iEmployeeService.create(user);
+    }
+
+    @GetMapping("res1/getAll")
+    public List<EmployeeDTO> getAll3(){
+        return iEmployeeService.getAll();
+    }
+
+    @PutMapping("res1/edit/{id}")
+    public EmployeeDTO edit3(@RequestBody EmployeeDTO user, @PathVariable Long id){
+        return iEmployeeService.edit(user, id);
+    }
+
+    @DeleteMapping("res1/delete/{id}")
+    public String delete3(@PathVariable Long id){
+        return iEmployeeService.delete(id);
+    }
+
+    @GetMapping("res1/clear")
+    public String clear(){
+        return iEmployeeService.clear();
+    }
+
 
 }
 
