56d331efc9bc4640f4d0a4d9a029461bd38c5ad3
Dhruv Agarwal <dhruv9837758333@gmail.com>
2025-03-07 10:44:10 +0530

    Added Rest API Using Service Layer

--- src/main/java/com/example/AddressBook/controllers/AddressController.java
+++ src/main/java/com/example/AddressBook/controllers/AddressController.java
@@ -2,11 +2,16 @@ package com.example.AddressBook.controllers;
 
 import com.example.AddressBook.dto.EmployeeDTO;
 import com.example.AddressBook.dto.ResponseDTO;
+import com.example.AddressBook.interfaces.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressController {
+
+    @Autowired
+    IEmployeeService iEmployeeService;
 
     //UC1 --> REST API handled using responseDTO(without interference of service layer)
 
@@ -34,4 +39,33 @@ public class AddressController {
     public ResponseDTO delete1(@PathVariable Long id){
         return new ResponseDTO("API triggered at /delete/{id}", "Success");
     }
+
+    //UC2 --> Handling REST API using Service layer without storing in DB
+
+    @GetMapping("/res/get/{id}")
+    public ResponseDTO get2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/get/{id}", "Success");
+    }
+
+    @PostMapping("/res/add")
+    public ResponseDTO create2(@RequestBody EmployeeDTO user){
+        return iEmployeeService.response("API triggered at /res/create", "Success");
+    }
+
+    @GetMapping("/res/getAll")
+    public ResponseDTO getAll2(){
+        return iEmployeeService.response("API triggered at /res/getAll", "Success");
+    }
+
+    @PutMapping("/res/edit/{id}")
+    public ResponseDTO edit2(@RequestBody EmployeeDTO user, @PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/edit/{id}", "Success");
+    }
+
+    @DeleteMapping("/res/delete/{id}")
+    public ResponseDTO delete2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/delete/{id}", "Success");
+    }
+
 }
+
