1d95c8e8990b5637e2c0a75d2d70484cd8c8256f
Dhruv Agarwal <dhruv9837758333@gmail.com>
2025-03-17 17:15:55 +0530

    Added Redis Authentication

--- src/main/java/com/example/AddressBook/controllers/AddressController.java
+++ src/main/java/com/example/AddressBook/controllers/AddressController.java
@@ -8,8 +8,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/addressbook")
--- src/main/java/com/example/AddressBook/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AddressBook.repositories;
-
-import com.example.AddressBook.entities.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
-
-    public EmployeeEntity findByEmail(String email);
-}
